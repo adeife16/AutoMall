f1ea963bd4b72f6d2188bd639f825a59b003468a
adeife16 <dadaadeife@gmail.com>
2022-10-21 07:58:03 +0100

    added login feature

--- login.php
+++ login.php
@@ -1,53 +1,42 @@
 <?php
-$title = "Login";
-require "header.php";
-if(isset($_SESSION['id']))
-{
-  ?>
-  <script type="text/javascript">window.location.replace('index.php');</script>
-  <?php
-}
+    $title = "Login";
+    require_once 'header.php';
 ?>
-<br>
-<br>
-<br>
-<section class="ads_area pt-70  ">
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-12-md-12-sm-12">
-        <div class="row justify-content-center">
-          <div class="col-md-6 col-lg-12 col-sm-8">
-            <div class="login-box-background">
-              <div class="login-box justify-content-center">
-                <h3 class="text-center">LOGIN</h3>
-                <form action="" id="login-form" class="form" method="post">
-                  <div class="form-group">
-                    <label for="login-email">Email</label>
-                    <input type="text" name="email" id="login-email" class="form-control" placeholder="Email Address" aria-describedby="helpId">
-                  </div>
-                  <div class="form-group">
-                    <label for="login-password">Password</label>
-                    <input type="password" name="password" id="login-password" class="form-control" placeholder="Password" aria-describedby="helpId">
-                  </div>
-                  <div class="form-group text-center mb-0">
-                    <button type="button" name="login" id="login" class="btn blue color-white" placeholder="LOGIN" aria-describedby="helpId">LOGIN</button>
-                    <button type="button" class="btn blue color-white hide" id="login-loader">&nbsp;&nbsp;<i class="fas fa-circle-notch fa-spin color-white"></i>&nbsp;&nbsp;</button>
-                  </div>
-                </form>
-                <br>
-                <div class="">
-                  <p class="text-right"><a href="forgot_pass.php" class="color-blue">forgot password?</a></p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+<style>
+    .message{
+        width: max-content;
+        border-radius: 10px;
+        position: absolute;
+        right: 20px;
+        top: 200px;
+        margin-right: 20px;
+    }
+</style>
+    <div class="message  red p-3">
+        <p class="color-white">Invalid login details</p>
     </div>
-  </div>
-</section>
-<br>
-<br>
-<br>
+    <div class="container-fluid pt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4 col-xl-4 col-sm-12">
+                <div class="login-box text-center red-border p-3" style="border-radius: 10px">
+                    <h2>LOGIN</h2>
+                    <form class="form" action="" id="login-form" method="post">
+                        <div class="form-group">
+                            <span for="email">Email</span>
+                            <input type="text" name="email" class="form-control" id="email" placeholder="Email" value="">
+                        </div>
+                        <div class="form-group">
+                            <span for="pass">Password</span>
+                            <input type="password" name="pass" id="pass" class="form-control" value="">
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn color-white btn-primary" name="login" id="login">LOGIN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
-require_once "footer.php";?>
+    require_once 'footer.php';
+?>
