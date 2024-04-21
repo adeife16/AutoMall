5e44f3006bbb0faef5632e34b722c2cd5187cfb9
Opeyemi Dada <dadaadeife@gmail.com>
2024-04-21 22:30:11 +0100

    Project Restructure

--- assets/js/login.js
+++
@@ -1,5 +0,0 @@
-// var sessionId = sessionStorage.getItem("session_id");
-// if(sessionId != ""){
-//   $(".preloader").removeClass("hide");
-//   window.location.replace('index.php');
-// }
--- js/login.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-});
-$("#login").click(function(event) {
-  event.preventDefault();
-
-  login();
-});
