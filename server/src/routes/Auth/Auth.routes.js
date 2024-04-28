1e9a725cc2a74dd8003b4fcd2abcd7d636187bd8
Opeyemi Dada <dadaadeife@gmail.com>
2024-04-28 21:29:50 +0100

    Authorization routes

---
+++ server/src/routes/Auth/Auth.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const authRoute = express.Router();
+const multer = require('multer');
+const upload = multer();
+
+const AuthController = require('../../controllers/Auth/Auth.controller');
+
+const authController = new AuthController();
+
+// Bind the methods to the authController instance
+const loginHandler = authController.login.bind(authController);
+const registerHandler = authController.register.bind(authController);
+const activateHandler = authController.activate.bind(authController);
+const refreshTokenHandler = authController.refreshToken.bind(authController);
+
+// Use the bound methods as route handlers
+authRoute.post('/auth/login', upload.none(), loginHandler);
+authRoute.post('/auth/register', upload.none(), registerHandler);
+authRoute.get('/auth/activate/token/:token', upload.none(), activateHandler);
+authRoute.get('/auth/refresh/token', upload.none(), refreshTokenHandler);
+
+module.exports = authRoute;
