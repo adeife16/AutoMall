830715f7752ed93f7a9464d21532ae2085e366ec
Opeyemi Dada <dadaadeife@gmail.com>
2024-07-13 11:32:18 +0100

    added toastify for notification

--- client/src/main.js
+++ client/src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.css'
+import Vue3Toasity from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(Vue3Toasity).use(router).mount('#app')
