415a84c4916ad99c9bc60ef8c4c2201dfc50bbd2
Opeyemi Dada <dadaadeife@gmail.com>
2024-07-13 11:35:39 +0100

    register auth routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import ActivateView from '../views/ActivateView.vue'
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/activate/:token',
+    name: 'activate',
+    component: ActivateView,
+    props: true
   }
 ]
 
