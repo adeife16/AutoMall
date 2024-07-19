07378032d0702df175ca355a0c500d77bbeff313
Opeyemi Dada <dadaadeife@gmail.com>
2024-07-19 18:19:25 +0100

    Added Mobile Navigation

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,27 +2,86 @@
   <div>
     <Navbar />
     <PreLoader />
-    <router-view/>
+    <router-view />
     <footer>
-      <h5>Mobile Nav here</h5>
+      <MobileNav />
     </footer>
   </div>
 </template>
 
-
 <script>
-import Navbar from './components/Navbar.vue';
-import PreLoader from './components/PreLoader.vue';
+import Navbar from "./components/Navbar.vue";
+import PreLoader from "./components/PreLoader.vue";
+import MobileNav from "./components/MobileNav.vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+import Cookies from "js-cookie";
+import axios from "axios"; // Import axios
+
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
-    PreLoader
-  }
-}
+    PreLoader,
+    MobileNav
+  },
 
+  setup() {
+    const router = useRouter();
+    const authStore = useAuthStore();
+    const apiUrl = process.env.VUE_APP_SERVER_API_URL; // Assuming you have this in your environment variables
+
+    const userRole = localStorage.getItem("role");
+
+    const accessToken = Cookies.get("accessToken");
+    const refreshToken = Cookies.get("refreshToken");
+
+    const refreshSession = async () => {
+      if (!refreshToken) {
+        return;
+      }
+
+      try {
+        const response = await axios.post(`${apiUrl}/auth/refresh/token`, {
+          refreshToken,
+        });
+
+        if (response.status === 200) {
+          const accessCookieLife = new Date(Date.now() + 12 * 60 * 60 * 1000); // 12 hours
+          Cookies.set("accessToken", response.data.accessToken, {
+            secure: true,
+            expires: accessCookieLife,
+          });
+
+          const refreshCookieLife = new Date(
+            Date.now() + 7 * 24 * 60 * 60 * 1000
+          ); // 7 days
+          Cookies.set("refreshToken", response.data.refreshToken, {
+            secure: true,
+            expires: refreshCookieLife,
+          });
+
+          authStore.login(response.data.user);
+        }
+      } catch (error) {
+        console.error("Failed to refresh session:", error);
+      }
+    };
+
+    if (!accessToken) {
+      refreshSession();
+    }
+
+    if (accessToken && refreshToken) {
+      const tokens = { accessToken, refreshToken };
+      authStore.persist({ tokens, userRole });
+    }
+
+    return {};
+  },
+};
 </script>
 
 <style>
-@import url('./styles/main.css');
+@import url("./styles/main.css");
 </style>
---
+++ client/src/components/MobileNav.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="mobile-nav lg:hidden fixed bottom-0 left-0 right-0 w-100 z-99">
+    <div class="nav-item">
+      <router-link to="/product">
+        <TagIcon class="icon size-4" />
+        <p>Products</p>
+      </router-link>
+    </div>
+    <div class="nav-item">
+      <router-link to="/wishlist">
+        <HeartIcon class="icon size-4" />
+        <p>Wishlist</p>
+      </router-link>
+    </div>
+    <div class="nav-item">
+      <router-link to="/sell">
+        <PlusIcon class="icon size-4" />
+        <p>Sell</p>
+      </router-link>
+    </div>
+    <div class="nav-item">
+      <router-link to="/profile">
+        <UserIcon class="icon size-4" />
+        <p>Profile</p>
+      </router-link>
+    </div>
+    <div class="nav-item">
+      <router-link to="/inbox">
+        <ChatBubbleBottomCenterTextIcon class="icon size-4" />
+        <p>Inbox</p>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/authStore";
+import { storeToRefs } from "pinia";
+import {
+  TagIcon,
+  ChatBubbleBottomCenterTextIcon,
+  PlusIcon,
+  HeartIcon,
+  UserIcon,
+} from "@heroicons/vue/24/outline";
+export default {
+  components: {
+    TagIcon,
+    ChatBubbleBottomCenterTextIcon,
+    PlusIcon,
+    HeartIcon,
+    UserIcon,
+  },
+  setup() {
+    const route = useRouter();
+  },
+};
+</script>
+
+<style scoped>
+.mobile-nav {
+  padding: 10px 0;
+  background-color: white;
+  color: gray;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.1);
+  margin-top: 20px;
+}
+.mobile-nav .nav-item {
+  flex: 1;
+  text-align: center;
+  padding: 10px 0;
+  font-size: 11px;
+  text-transform: uppercase;
+}
+@media (min-width: 768px) {
+  .mobile-nav {
+    display: none;
+  }
+}
+</style>
